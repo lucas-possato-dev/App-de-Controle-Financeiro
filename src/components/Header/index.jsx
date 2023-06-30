ab76dd832c11c2a56d71d6793887216e15a66de2
lucas-possato-dev <lucaspstinho@gmail.com>
2023-06-30 13:51:42 -0300

    projeto finalizado

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Container, HeaderT, Title } from "./styles";
+
+const Header = () => {
+  return (
+    <Container>
+      <HeaderT>
+        <Title>Controle Financeiro</Title>
+      </HeaderT>
+    </Container>
+  );
+};
+
+export default Header;
